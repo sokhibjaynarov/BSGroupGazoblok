1ee89dce1ec89865f84ff5683e96284e5ca19d98
Sokhib Jaynarov <s.jaynarov@baik.uz>
2023-02-11 21:48:39 +0500

    add function multi language

--- js/main.js
+++ js/main.js
@@ -4,28 +4,150 @@ var arrLang = {
     "navbar_home": "Bosh sahifa",
     "navbat_about": "Biz haqimizda",
     "navbar_service": "Xizmatlar",
+    "navbat_benifits": "Afzalliklari",
+    "navbat_product": "Mahsulotlar",
+    "navbat_contact": "Aloqa",
+    "navbat_faq": "Savol-Javob",
 
     // home section
+    "home_title": "BS GAZOBLOK",
+    "home_subtitle": "Galandiyaning aircrete texnologiyasi",
 
     // about section
-    "about_heading": "Biz haqimizda"
+    "about_heading": "Biz haqimizda",
+    "about_content_title": "BS Gazobloklari Galandiyaning AIRCRETE texnologiyasi bilan!",
+    "about_content_subtitle": "BSGroup kompaniyasi O'zbekiston hududida avtoklavlangan gazoblokini ishlab chiqarish bo'yicha yetakchi hisoblanadi. Zavodimiz 2023-yilda tashkil etilgan, zavod yiliga 450,000 kubometr mahsulot ishlab chiqarish quvvatiga ega. BSGroup Galandiyaning AIRCRETE ishlab chiqarish texnologiyalari bilan jihozlangan zamonaviy zavod bo'lib, avtoklavda mustahkamlashgan gazobloklarini ishlab chiqaradi.",
 
     // service section
+    "service_title": "Bizning xizmatlar",
+    "service_subtitle": "Biz sizga yuqori Jahon standartlariga javob beruvchi mustahkam, ekologik toza mahsulotlarni ishlab chiqaramiz va yetkazib beramiz.",
+    "service_item_deliver_title": "Yetkazib berish",
+    "service_item_deliver_subtitle": "Biz sizga materiallarni turli transport vositalari, temir yo’l, yuk mashinalari, turli yuk ko’tarish quvvatiga ega kran-plitalar orqali etkazib bera olamiz.",
+    "service_item_online_title": "Onlayn buyurtma berish",
+    "service_item_online_subtitle": "Onlayn buyrtma berish xizmati mavjud, siz veb-saytimiz orqali buyurtma berishingiz mumkin, kerakli tovarlar hajmini va kontaktlaringizni ko’rsatasiz, menejerimiz siz bilan bog’lanadi. Siz tovarlar, xizmatlar uchun masofadan turib to’lashingiz mumkin va mahsulotlarimiz sizning qurilish maydonchangizga yetkazib beriladi.",
+    "service_item_calculate_title": "Materiallar hajmlarini hisoblash",
+    "service_item_calculate_subtitle": "Bizga murojaat qiling va biz sizga qurilish uchun qancha gazoblok kerakligini aniqlashga yordam beramiz.",
 
     // counter section
+    "counter_work_name": "Ish vaqti",
+    "counter_project_name": "Completed Projects",
+    "counter_worker_name": "Ishchi xodimlar",
+    "counter_volume_name": "Yillik ishlab chiqarish hajmi",
 
-    // project section
+    // benifits section
+    "benifits": "BS Gazobloklarining afzalliklari",
+    "benifits_speed": "Qurulish tezligi",
+    "benifits_rigth": "Aniqlik",
+    "benifits_fire": "Yong'inga qarshilik",
+    "benifits_eco": "Ekologik toza",
+    "benifits_scale": "Yengil vazn",
+    "benifits_voice": "Ovoz izolyatsiyasi",
+
+    // photogalary section
+    "photo_galary": "Fotogalareya",
+
+    // product section
+    "product_title": "Mahsulotlar",
+
+    // contact section
+    "contact_heading": "Biz bilan bog'laning",
+
+    // faq section
+    "faq_heading": "Eng ko'p so'ralgan savollar",
+
+    // footer section
+    "footer_heading_about": "Biz haqimizda",
+    "footer_home": "Bosh sahifa",
+    "footer_about": "Biz haqimizda",
+    "footer_service": "Xizmatlar",
+    "footer_benifits": "Afzalliklari",
+    "footer_product": "Mahsulotlar",
+    "footer_contact": "Aloqa",
+    "footer_faq": "Savol-Javob",
+    "link": "Foydali havolalar",
+    "footer_contact": "Biz bilan bog'laning",
+    "footer_address_1": "Toshkent shahar",
+    "footer_address_2": "Yakkasaroy tumani",
+    "footer_address_3": "Cho'pon ota ko'chasi 17-uy",
+    "footer_phone_number": "Telefon raqam:",
+    "footer_email": "Email:",
+    "copyright": "© Mualliflik huquqi 2023. Barcha huquqlar himoyalangan.",
+
   },
   "ru-ru": {
     // navbar section
     "navbar_home": "Главная страница",
     "navbat_about": "О нас",
-    "navbar_service": "Связь",
+    "navbar_service": "Услуги",
+    "navbat_benifits": "Преимущества",
+    "navbat_product": "Продукты",
+    "navbat_contact": "Связь",
+    "navbat_faq": "Вопросы И Ответы",
 
     // home section
+    "home_title": "BS GAZOBLOK",
+    "home_subtitle": "Галандиянинг воздухобетонные технологии",
 
     // about section
-    "about_heading": "О нас"
+    "about_heading": "О нас",
+    "about_content_title": "Газоблоки BS с технологией GALANDIA Aircrete!",
+    "about_content_subtitle": "Компания Bsgroup является лидером по производству автоклавированных газоблоков на территории Узбекистана. Наш завод был основан в 2023 году, производственная мощность завода составляет 450 000 кубометров продукции в год. BSGroup-это современный завод, оснащенный технологиями производства GALANDIA AIRCRETE,который производит автоклавные газоблоки.",
+
+    // service section
+    "service_title": "Наши услуги",
+    "service_subtitle": "Мы производим и поставляем вам прочные, экологически чистые продукты, соответствующие высоким мировым стандартам.",
+    "service_item_deliver_title": "Доставка",
+    "service_item_deliver_subtitle": "Мы можем доставить вам материалы с помощью различных транспортных средств, рельсов, грузовиков, кранов-пластин с различной грузоподъемностью.",
+    "service_item_online_title": "Онлайн-заказ",
+    "service_item_online_subtitle": "Есть сервис онлайн-заказа, заказать можно через наш сайт, укажите необходимый объем товара и ваши контакты, с вами свяжется наш менеджер. Вы можете оплачивать товары, услуги удаленно, и наша продукция будет доставлена на вашу строительную площадку.",
+    "service_item_calculate_title": "Расчет объемов материалов",
+    "service_item_calculate_subtitle": "Свяжитесь с нами, и мы поможем вам определить, сколько газоблоков вам понадобится для строительства.",
+
+    // counter section
+    "counter_work_name": "Рабочее время",
+    "counter_project_name": "Completed Projects",
+    "counter_worker_name": "Рабочий персонал",
+    "counter_volume_name": "Годовой объем производства",
+
+    // benifits section
+    "benifits": "Преимущества газоблоков BS",
+    "benifits_speed": "Qurulish tezligi",
+    "benifits_rigth": "Точность",
+    "benifits_fire": "Огнестойкость",
+    "benifits_eco": "Экологически чистый",
+    "benifits_scale": "Легкий вес",
+    "benifits_voice": "Звукоизоляция",
+
+    // photogalary section
+    "photo_galary": "Фотогалерея",
+
+    // product section
+    "product_title": "Продукты",
+
+    // contact section
+    "contact_heading": "Свяжитесь с нами",
+
+    // faq section
+    "faq_heading": "Наиболее часто задаваемые вопросы",
+
+    // footer section
+    "footer_home": "Главная страница",
+    "footer_about": "О нас",
+    "footer_service": "Услуги",
+    "footer_benifits": "Преимущества",
+    "footer_product": "Продукты",
+    "footer_contact": "Связь",
+    "footer_faq": "Вопросы И Ответы",
+    "footer_heading_about": "О нас",
+    "link": "Полезные ссылки",
+    "footer_contact": "Свяжитесь с нами",
+    "footer_address_1": "Город Ташкент",
+    "footer_address_2": "Якасарайский район",
+    "footer_address_3": "Улица Чўпон ота, 17",
+    "footer_phone_number": "Номер телефона:",
+    "footer_email": "Email:",
+    "copyright": "© Авторские права 2023. Все права защищены.",
+
   }
 };
 
